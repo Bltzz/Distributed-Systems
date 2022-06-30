3782890e41416aafa1bf0e428c4c697e31851db6
Bltzz <jonaspfaff25@gmail.com>
2022-06-30 22:12:47 +0200

    Try to fix heartbeat
    - not fully functional.
    - Remove tupel from list doesnt work as expected
     -- ValueError: list.remove(x): x not in list
    
    ## NOTE:
    Voting sometimes works, sometimes not -> depending on who starts it when, not sure...

--- src/Middleware/main.py
+++ src/Middleware/main.py
@@ -1,11 +1,8 @@
-
-from ipaddress import ip_address
 import socket
 from threading import Thread
 import time
 import sys
 import uuid
-import argparse
 from json import loads, dumps
 import threading
 import os
@@ -13,13 +10,32 @@ import os
 from CommonUtil import encodeMessage, decodeMessage, getBroadcastIP, IP_ADDR
 
 
+
 #Global var if this Peer is the leader - set to True after Voting
 leader = False
+leaderIpAndUUID = (None, None)
 
 #List of Peers Tupel(ip, uuid)
 peers = []
 
 UUID = uuid.uuid4()
+
+def sortList():
+    sortedList = sorted(peers, key=lambda peers: peers[0])
+    #print("Sorted List: ", sortedList)
+    #("192.168.172.xxx", str(UUID))
+    return sortedList
+# needed for heartbeat and voting
+def findRightNeighbor(ip_address):
+    sortedList = sortList()
+    myIndex = sortedList.index((ip_address, str(UUID)))
+    return sortedList[(myIndex + 1) % len(sortedList)] # use modulo to implement the list as ring
+
+# needed for heartbeat and voting
+def findLeftNeighbor(ip_address):
+    sortedList = sortList()
+    myIndex = sortedList.index((ip_address, str(UUID)))
+    return sortedList[(myIndex - 1) % len(sortedList)] # use modulo to implement the list as ring
 
 
 class BroadcastListener(Thread):
@@ -29,7 +45,7 @@ class BroadcastListener(Thread):
         self.my_host = socket.gethostname()
         self.my_ip = IP_ADDR # from CommonUtil # socket.gethostbyname(self.my_host)
         self.UUID = UUID
-        print("My IP: "+self.my_ip)
+        #print("My IP: "+self.my_ip)
 
         # Create a UDP socket
         self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,14 +56,14 @@ class BroadcastListener(Thread):
             socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # Bind socket to address and port
         self.listen_socket.bind(('', self.bcport))
-        print("Listening to broadcast messages")
+        #print("Listening to broadcast messages")
         self.running = True
 
     def run(self):
         while self.running:
             data, addr = self.listen_socket.recvfrom(1024)
             if data:
-                print("Received broadcast message:", decodeMessage(data))
+                #print("Received broadcast message:", decodeMessage(data))
                 newUDPThread = UDPBroadcastHandler(data)
                 newUDPThread.start()
 
@@ -75,7 +91,7 @@ class SendMessage():
             "uuid": str(UUID),
             "msg": message
         }
-        print(self.msg)
+        #print(self.msg)
 
         self.broadcast(self.bcip, self.bcport, self.msg)
 
@@ -100,7 +116,7 @@ class BroadcastSender():
             "uuid": str(UUID),
             "msg": my_ip
         }
-        print(self.msg)
+        #print(self.msg)
 
         self.broadcast(self.bcip, self.bcport, self.msg)
 
@@ -125,17 +141,17 @@ class TCPUnicastSender():
         self.hostname = socket.gethostname()
         self.ip_address = IP_ADDR
         self.msg = msg
-        print("SEND TCP Message: ", self.msg, self.recipientIP)
+        #print("SEND TCP Message: ", self.msg, self.recipientIP)
         self.sendMessage(self.recipientIP, self.uport, self.msg)
 
     def sendMessage(self, recipientIP, uport, message):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(recipientIP, uport)
+        #print(recipientIP, uport)
         s.connect_ex((recipientIP, uport))
         s.sendall(encodeMessage(message))
         data = s.recv(1024)
         s.close()
-        print('TCP Received: ', repr(data))
+        #print('TCP Received: ', repr(data))
 
 class MessageInterpreter():
     def __init__(self, data):
@@ -148,11 +164,10 @@ class MessageInterpreter():
         #self.conn = conn
 
         if(self.command == 'INIT'):
-            peers.append((self.ip_addr, self.id))
             if(self.ip_addr == self.my_ip_addr ):
 
                 # Wenn Broadcast Nachricht zurück zum Broadcast Sender geht wird keine TCP verschickt
-                print("Es wird keine TCP Nachricht verschickt")
+                #print("Es wird keine TCP Nachricht verschickt")
                 time.sleep(2)
 
                 # Der neue joiner startet ein Voting
@@ -160,7 +175,8 @@ class MessageInterpreter():
                 vote.startVote()
 
             else:
-                print(self.my_id)
+                peers.append((self.ip_addr, self.id))
+                #print(self.my_id)
                 res = {
                     "cmd": "INIT_RESPONSE",
                     "uuid": str(self.my_id),
@@ -182,23 +198,24 @@ class MessageInterpreter():
         if(self.command == 'HEARTBEAT'):
             pass
         if(self.command == 'LOST_NEIGHBOR'):
+            peerToRm = (self.ip_addr, self.id)
+            peers.remove(peerToRm)
+            #if len(peers) < 3 :
+             #   Game.stop()
             # drop from list
             # check if list <=3
             # (stop game, wait for new peer)
             # (else: send updated list via Broadcast)
             pass
-        if(self.command == 'Not LEader'): #?????
-            leader = False
-            print("Peer")
 
     def removePeerFromList(self, ip_addr, id):
         peers.remove((ip_addr, id))
-        print(peers)
+        #print(peers)
             
 
     def addPeerToList(self, ip_addr, id):
         peers.append((ip_addr, id))
-        print("Peers: ", peers)
+        #print("Peers: ", peers)
 
 class TCPUnicastListener(Thread):
     def __init__(self, listeningPort, UUID):
@@ -218,7 +235,7 @@ class TCPUnicastListener(Thread):
             ##print("True")
             s.listen(1)
             conn, addr = s.accept()
-            print("New connection added: ", addr)
+            #print("New connection added: ", addr)
             newthread = TCPUnicastHandler(conn, addr, self.UUID, self.ip_addr) #missing: execute in a new thread
             newthread.start()
 
@@ -231,18 +248,18 @@ class TCPUnicastHandler(Thread):
         self.UUID = UUID
         self.ip_addr = ip_addr
 
-        print("New connection added: ", self.addr)
+        #print("New connection added: ", self.addr)
 
     
     def run(self):
-        print("Connection from: ", self.addr)
+        #print("Connection from: ", self.addr)
 
         while True:
             data = self.conn.recv(1024)
             if not data:
                 break
             #msg = decodeMessage(data)
-            print("Unicast Message received: ", data.decode())
+            #print("Unicast Message received: ", data.decode())
             
             msg = {
                 "cmd": "SUCCESS",
@@ -252,7 +269,7 @@ class TCPUnicastHandler(Thread):
             self.conn.sendall(encodeMessage(msg))
             MessageInterpreter(data)
             #conn.sendall(str.encode("Thanks"))
-        print("Peer at ", self.addr ," disconnected...")
+        #print("Peer at ", self.addr ," disconnected...")
 
 class HeartbeatListener(Thread):
     def __init__(self, heartbeat_port, UUID):
@@ -262,7 +279,7 @@ class HeartbeatListener(Thread):
         self.ip_addr = IP_ADDR
         self.UUID = UUID
 
-        print("Heartbeat starting...")
+        #print("Heartbeat starting...")
 
         print("Heartbeat at ", self.ip_addr ," listening on port ", self.heartbeat_port)
         self.running = True
@@ -275,7 +292,7 @@ class HeartbeatListener(Thread):
 
         while self.running:
             conn, addr = s.accept()
-            data = conn.rcv(1024)
+            data = conn.recv(1024)
 
             if not data:
                 break
@@ -291,35 +308,81 @@ class HeartbeatListener(Thread):
 
         conn.close()
 
-class HeartbeatSender():
-    def __init__(self, UUID, recipientPeer):
+class HeartbeatSender(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.running = True
         self.uport = 59071
         self.UUID = UUID
-        self.recipientIP = recipientPeer[0]
-        self.recipientUUID = recipientPeer[1]
         self.hostname = socket.gethostname()
         self.ip_address = IP_ADDR
+        self.counterLeft = 0
+        self.counterRight = 0
+        self.rightNeighbor = findRightNeighbor(self.ip_address)
+        self.leftNeighbor = findLeftNeighbor(self.ip_address)
+        #self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.recipientIP = recipientPeer[0]
+        #self.recipientUUID = recipientPeer[1]
         self.msg = {
             "cmd": "HEARTBEAT",
             "uuid": str(self.UUID),
             "msg": self.ip_address
         }
-        print("SEND TCP Message: ", self.msg, self.recipientIP)
-        self.sendMessage(self.recipientIP, self.uport, self.msg)
+        #print("SEND TCP Message: ", self.msg, self.recipientIP)
+        #self.sendMessage(self.recipientIP, self.uport, self.msg)
 
-    def sendMessage(self, recipientIP, uport, message):
-        ## try
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(recipientIP, uport)
-        s.connect_ex((recipientIP, uport))
-        s.sendall(encodeMessage(message))
-        data = s.recv(1024)
-        s.close()
-        print('TCP Received: ', repr(data))
-        ## expect socket.error: 
-        ### counter ++
-        ### pass
+    def run(self):
+        self.sendMessage(self.rightNeighbor, self.uport, self.msg, "right")
+        time.sleep(2)
+        self.sendMessage(self.leftNeighbor, self.uport, self.msg, "left")
+        time.sleep(2)
+        pass
+       
 
+    def sendMessage(self, neighbor, uport, message, direction):
+        while self.running:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                #print(neighbor[0], uport)
+                s.connect_ex((neighbor[0], uport))
+                s.sendall(encodeMessage(message))
+                data = s.recv(1024)
+                time.sleep(1)
+                if not data:
+                    if direction == "right": 
+                        self.counterRight += 1
+                    elif direction == "left":
+                        self.counterLeft += 1
+                    break
+                print('TCP Received: ', repr(data))
+            except socket.error or Exception:
+                if direction == "right": 
+                    self.counterRight += 1
+                elif direction == "left":
+                    self.counterLeft += 1
+            finally:
+                s.close
+
+            if self.counterLeft > 3 :
+                print(neighbor)
+                peers.remove(neighbor)
+                self.leftNeighbor = findLeftNeighbor(self.ip_address)
+                self.sendLostPeerMessage(neighbor)
+                pass
+            if self.counterRight > 3 :
+                print(neighbor)
+                peers.remove(neighbor)
+                self.rightNeighbor = findRightNeighbor(self.ip_address)
+                self.sendLostPeerMessage(neighbor)
+        pass
+
+    def sendLostPeerMessage(self, neighbor):
+        msgLostPeer = {
+                    "cmd": "LOST_NEIGHBOR",
+                    "uuid": neighbor[1],
+                    "msg": neighbor[0]
+                }                
+        TCPUnicastSender(self.UUID, self.leftNeighbor[0], msgLostPeer)
         ## if counter > 2 : communicate Lost peer
         #TCPUnicastSender(UUID, leaderIP, {"cmd": "LOST_NEIGHBOR"})
         # Leader: Broadcast -> updated player list
@@ -330,30 +393,13 @@ class Voting():
         self.ip_address = IP_ADDR
         self.UUID = str(UUID)
         self.isLeaderElected = False
-        self.sortedList = self.sortList()
-        self.rightNeighbor = self.findRightNeighbor()
-        self.leftNeighbor = self.findLeftNeighbor()
+        self.isLeader = False
+        self.sortedList = sortList()
+        self.rightNeighbor = findRightNeighbor(self.ip_address)
+        self.leftNeighbor = findLeftNeighbor(self.ip_address)
         pass
 
-    def sortList(self):
-        self.sortedList = sorted(peers, key=lambda peers: peers[0])
-        print("Sorted List: ", self.sortedList)
-        #("192.168.172.xxx", str(UUID))
-        return self.sortedList
 
-    # also needed for heartbeat
-    def findRightNeighbor(self):
-        myIndex = self.sortedList.index((self.ip_address, self.UUID))
-        return self.sortedList[(myIndex + 1) % len(self.sortedList)] # use modulo to implement the list as ring
-    
-    # also needed for heartbeat
-    def findLeftNeighbor(self):
-        myIndex = self.sortedList.index((self.ip_address, self.UUID))
-        return self.sortedList[(myIndex - 1) % len(self.sortedList)] # use modulo to implement the list as ring
-
-    def validateIncomingMessage(self):
-        # do the checking and pass the higher UUID. 
-        pass  
 
     def startVote(self):
         # needs msg as arg - otherwise we have conflicts! 
@@ -368,6 +414,7 @@ class Voting():
         pass
 
     def respondWithLCRAlgorithmToVote(self, msg):
+        print("Incoming Voting: ", msg)
         receivedUUID = msg["uuid"]
         receivedIP = msg["msg"]
         isLeaderElected = msg["leaderElected"]
@@ -376,16 +423,19 @@ class Voting():
                 self.isLeaderElected = True
                 leader = False
                 # TODO: where to put leader ip?
-                leaderIp = (msg["msg"],msg["uuid"])
+                leaderIpAndUUID = (msg["msg"],msg["uuid"])
+                print("My election forward response: ", msg)
                 TCPUnicastSender(self.UUID, self.leftNeighbor[0], msg)
         else:
             if self.UUID == receivedUUID:
+                leader = True
                 response = {
                     "cmd": "VOTING",
                     "uuid": self.UUID,
                     "msg": self.ip_address,
                     "leaderElected" : True
                 }
+                print("I am Leader: ", msg)
                 TCPUnicastSender(self.UUID, self.leftNeighbor[0], response)
             elif self.isOwnUuidIsHigher(self.UUID, receivedUUID):
                 # probably ignore this elif
@@ -395,8 +445,10 @@ class Voting():
                     "msg": self.ip_address,
                     "leaderElected" : False
                 }
+                print("Replacing incoming uuid and ip with my stats, because I am higher", msg)
                 TCPUnicastSender(self.UUID, self.leftNeighbor[0], response)
             else:
+                print("Forward incoming Voting", msg)
                 TCPUnicastSender(self.UUID, self.leftNeighbor[0], msg)
         pass
 
@@ -426,11 +478,14 @@ class Game():
 
     def startGame(self):
         print("Game start...")
+        pass
         # game code
 
 
 if __name__ == '__main__':
     try:
+        peers.append((IP_ADDR, str(UUID)))
+        print(peers)
         # Broadcast Listener
         print("Start TCP Unicast Listener")
         listener = TCPUnicastListener(59072, UUID) #TCP UnicastListener
@@ -438,6 +493,9 @@ if __name__ == '__main__':
 
         BListener = BroadcastListener()
         BListener.start()
+
+        heartbeat = HeartbeatListener(59071, UUID)
+        heartbeat.start()
 
         time.sleep(5)
         # Broadcast Sender
@@ -449,19 +507,32 @@ if __name__ == '__main__':
 
         while len(peers) < 3:
             time.sleep(1)
-        '''
+
         time.sleep(3)
 
         input("Write 'start' to start the game: ")
 
-        vote = Voting()
-        vote.startVote()
+        #vote = Voting()
+        #vote.startVote()
 
+        time.sleep(3)
+        if leader:
+            print("I AM THE LEADER!!!!!!!!!!!!!!")
+        else:
+            print("I am only another Peer in the ring!")
+        heartbeatSender = HeartbeatSender()
+        heartbeatSender.start()
+
+        '''
         #TODO: Loop untill voting.lead_is_elected == True: Then start listening for heartbeats
+        while vote.isLeaderElected == True:
+            time.sleep(2)
+            heartbeatSenderLeft = HeartbeatSender(UUID, vote.leftNeighbor)
+            heartbeatSenderRight = HeartbeatSender(UUID, vote.rightNeighbor)
 
-        heartbeat = HeartbeatListener(59071, UUID)
-        heartbeat.start()
 
+        
+        
         # neuer Thread!
         time.sleep(2)
         while True: 
@@ -474,14 +545,10 @@ if __name__ == '__main__':
         if leader == True:
             game = Game()
             game.checkPlayer()
-
+        '''
 
         # Heartbeat (TCP Unicast)
         #heartbeat = HeartbeatListener(59071, UUID)
         #heartbeat.start()
-        '''
-
-
-
     except KeyboardInterrupt:
         sys.exit(0)
